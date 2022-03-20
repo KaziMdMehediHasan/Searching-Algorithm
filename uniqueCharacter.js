72dec3a4ef059c3c1b817b925915f98bd010a70a
KaziMdMehediHasan <=>
2022-03-20 14:53:12 +0600

    unique characters solution

---
+++ uniqueCharacter.js
@@ -0,0 +1,18 @@
+function isUnique(str) {
+  let obj = {};
+  if (str === "") {
+    return "Invalid String";
+  }
+  for (i = 0; i < str.length; i++) {
+    if (obj[str[i]] === undefined) {
+      obj[str[i]] = 1;
+    } else {
+      return false;
+    }
+  }
+  return true;
+}
+
+// uniqueWord('Hello');
+
+console.log(isUnique("Type"));
